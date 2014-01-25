37761dfd78fcf507a8c15cf012db6b6a392b2fee
Lee Bannard <l_bannard@yahoo.co.uk>
2014-01-25 22:06:09 +0000

    setup script fixes

--- mysetup.sh
+++ mysetup.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 getNeobundle() {
-    mkdir -p ~/.vim/bundle
-    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+    if [ -d ~/.vim/bundle/neobundle.vim ]; then
+        echo "neoBundle already downloaded"
+    else
+        mkdir -p ~/.vim/bundle
+        git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+    fi
 }
 
 copyDotfiles() {
+    echo "copying dotfiles into user directory"
     cd ~/dotfiles
-    cp .vim ~
+    cp .vimrc ~
     cp .tmux.conf ~
     cp .gitconfig .gitignore_global .githelpers ~
 }
 
-copyDotfiles()
-getNeobundle()
+getNeobundle
+copyDotfiles
