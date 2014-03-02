65d234eeed1a8eff7709dca7482678495bace1f3
Lee Bannard <l_bannard@yahoo.co.uk>
2014-03-02 19:22:47 +0000

    add zsh install to mysetup.sh

--- mysetup.sh
+++ mysetup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-getNeobundle() {
-    if [ -d ~/.vim/bundle/neobundle.vim ]; then
-        echo "neoBundle already downloaded"
-    else
+get_neobundle() {
+    if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
+        echo "downloading neoBundle..."
         mkdir -p ~/.vim/bundle
         git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
     fi
 }
 
-copyDotfiles() {
+copy_dotfiles() {
     echo "copying dotfiles into user directory"
     cd configfiles
     for file in *; do
@@ -19,5 +18,23 @@ copyDotfiles() {
     done
 }
 
-getNeobundle
-copyDotfiles
+vim_swap_and_backup_dirs() {
+    if [ ! -d ~/.vim/backup ]; then
+        echo "creating vim backup dir"
+        mkdir -p ~/.vim/backup/
+    fi
+    if [ ! -d ~/.vim/swap ]; then
+        echo "creating vim swap dir"
+        mkdir ~/.vim/swap/
+    fi
+}
+
+install_oh_my_zsh() {
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+    chsh -s /bin/zsh
+}
+
+vim_swap_and_backup_dirs
+get_neobundle
+copy_dotfiles
