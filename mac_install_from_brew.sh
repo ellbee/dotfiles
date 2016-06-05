6d12ce2ce37c9d5802cfa27a05f543f82a485554
Lee Bannard <l_bannard@yahoo.co.uk>
2016-06-05 20:31:18 +0100

    brew: update list of initial packages to install

--- mac_install_from_brew.sh
+++ mac_install_from_brew.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-programs=(ag git neovim node nvm pstree ranger
-  reattach-to-user-namespace rlwrap tmux trash tree vim xz zsh
+programs=(
+  ag elixir git neovim node nvm pstree ranger
+  rlwrap tmux trash tree vim xz zsh
 )
 
 for program in "${programs[@]}"; do
