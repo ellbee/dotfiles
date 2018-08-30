b263d8f99e12a13ff366e5ab2633d1e5e84d867f
Lee Bannard <l_bannard@yahoo.co.uk>
2018-08-30 21:19:39 +0100

    zsh: install asdf directly

--- mac_setup.sh
+++ mac_setup.sh
@@ -20,6 +20,9 @@ install_asdf_plugins() {
 
 disable_path_helper
 change_delete_code_from_ctrl_h_to_ascii_del
-install_asdf_plugins
 
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.1
+source $HOME/.asdf/asdf.sh
+install_asdf_plugins
