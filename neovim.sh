5764b9425bc6f9de3158332a4c6a8ef1972a7531
Lee Bannard <l_bannard@yahoo.co.uk>
2022-06-28 13:06:51 +0100

    Update various config files

--- neovim.sh
+++ neovim.sh
@@ -5,6 +5,3 @@ ln -s ~/.vimrc ~/.vim/init.vim
 
 curl -fLo ~/./config/nvim/autoload/plug.vim --create-dirs \
   https://raw.github.com/junegunn/vim-plug/master/plug.vim
-
-sudo pip2 install neovim
-sudo pip3 install neovim
