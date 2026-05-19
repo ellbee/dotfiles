2530ea336889f57128a6ed2c3a415d9b4b80a068
Lee Bannard <l_bannard@yahoo.co.uk>
2026-05-19 13:02:50 +0100

    vim: remove outdated neovim setup script

--- neovim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ln -s ~/.vim ${XDG_CONFIG_HOME:=~/.config/nvim}
-ln -s ~/.vimrc ~/.vim/init.vim
-
-curl -fLo ~/./config/nvim/autoload/plug.vim --create-dirs \
-  https://raw.github.com/junegunn/vim-plug/master/plug.vim
